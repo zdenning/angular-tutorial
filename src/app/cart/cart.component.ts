34591f90cbfd44c831fc382cbc47b041d2ae5490
zdenning <zacdenning@gmail.com>
2020-12-08 15:04:40 -0500

    finished form input

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from "@angular/core";
+import { FormBuilder } from "@angular/forms";
+
 import { CartService } from "../cart.service";
 
 @Component({
@@ -8,10 +10,25 @@ import { CartService } from "../cart.service";
 })
 export class CartComponent implements OnInit {
   items;
+  checkoutForm;
 
-  constructor(private cartService: CartService) {}
+  constructor(
+    private cartService: CartService,
+    private formBuilder: FormBuilder
+  ) {
+    this.checkoutForm = this.formBuilder.group({
+      name: "",
+      address: ""
+    });
+  }
 
   ngOnInit() {
     this.items = this.cartService.getItems();
+  }
+
+  onSubmit(customerData) {
+    this.items = this.cartService.clearCart();
+    this.checkoutForm.reset();
+    console.warn("Your order has been submitted", customerData);
   }
 }
